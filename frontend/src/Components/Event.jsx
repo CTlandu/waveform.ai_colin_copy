821fbfe6cefef0aa7499379913f8dc3291033918
AlanGonzalez10123 <alangonzalezosorio04@gmail.com>
2025-03-02 13:12:03 -0500

    Made image fit in card container

--- frontend/src/Components/Event.jsx
+++ frontend/src/Components/Event.jsx
@@ -7,7 +7,7 @@ const Event = ({ event, onClick }) => {
         <img
           src={event.image || "https://via.placeholder.com/300"} // Default placeholder image
           alt={event.title}
-          className="w-full h-40 object-cover rounded-md"
+          className="w-full h-40 object-contain rounded-md"
         />
         <h3 className="text-lg font-semibold text-cyan-900 mt-2">{event.title}</h3>
       </div>
